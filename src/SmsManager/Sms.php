b6100ae0407ae718f0aef0b441511e45f616866d
sumbang <tsumbang@gmail.com>
2019-11-25 15:12:28 +0100

    first commit

---
+++ src/SmsManager/Sms.php
@@ -0,0 +1,50 @@
+<?php
+   namespace SmsManager;
+   
+   class Sms {
+
+      public static function sendByLMT($numero,$message,$source,$username,$password) {
+
+        $url = 'http://lmtgroup.dyndns.org/sendsms/sendsmsGold.php?';
+                    $timeout = 10;
+                    $destination = $numero;
+                    $request = $url . "UserName=" . urlencode($username) . "&Password=" . urlencode($password) . "";
+                    $request .= "&SOA=" . urlencode($source) . "&MN=" . urlencode($destination) . "&SM=" . urlencode($message);
+
+                    $url = $request;
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_POST, 1);
+
+                    curl_exec($ch);
+                    curl_close($ch);
+
+                    return 'success';
+      }
+
+      public static function sendByMTARGET($numero,$message,$source,$username,$password) {
+
+                    $destination = $numero;
+                    
+                    $message = urlencode($message);
+
+
+    
+                    $url = 'https://api-public.mtarget.fr/api-sms.json?username='.$username.'&password='.$password.'&sender='.$source.'&msisdn=%2b237'.$destination.'&msg='.$message;
+        
+                    $ch = curl_init();
+            
+                    curl_setopt($ch, CURLOPT_URL, $url);
+            
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            
+                    curl_exec($ch);
+        
+                    curl_close($ch);
+
+                    return 'success';
+     }
+     
+   }
+?>
