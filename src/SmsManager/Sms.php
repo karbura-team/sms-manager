fe85253c42da4074e0c150d0c4c8975c6a65d422
Sumbang <tsumbang@outlook.com>
2022-04-06 12:50:13 +0100

    update

--- src/SmsManager/Sms.php
+++ src/SmsManager/Sms.php
@@ -44,6 +44,40 @@
 
                     return 'success';
      }
+
+       public static function sendByNexhas($numero,$message,$source,$username,$password) {
+
+           $destination = $numero;
+
+           $curl = curl_init();
+
+           curl_setopt_array($curl, array(
+               CURLOPT_URL => 'https://smsvas.com/bulk/public/index.php/api/v1/sendsms',
+               CURLOPT_RETURNTRANSFER => true,
+               CURLOPT_ENCODING => '',
+               CURLOPT_MAXREDIRS => 10,
+               CURLOPT_TIMEOUT => 0,
+               CURLOPT_FOLLOWLOCATION => true,
+               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+               CURLOPT_CUSTOMREQUEST => 'POST',
+               CURLOPT_POSTFIELDS =>'{
+                  "user": "'.$username.'",
+                  "password": "'.$password.'",
+                  "senderid": "'.$source.'",
+                  "sms": "'.$message.'",
+                  "mobiles": "'.$destination.'"
+              }',
+               CURLOPT_HTTPHEADER => array(
+                   'Content-Type: application/json'
+               ),
+           ));
+
+           $response = curl_exec($curl);
+
+           curl_close($curl);
+
+           return 'success';
+       }
      
    }
 ?>
