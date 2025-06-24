f1165afd224850e0d34d352d077a283e480c0175
Daniel SAKOU <daniel.sakou@karbura.com>
2025-06-24 09:20:26 +0100

    Changed MTarget to Nexah

--- src/SmsManager/Sms.php
+++ src/SmsManager/Sms.php
@@ -1,83 +1,71 @@
 <?php
 
-   namespace SmsManager;
-   
-   class Sms {
+namespace SmsManager;
 
-      public static function sendByLMT($numero,$message,$source,$username,$password) {
+class Sms
+{
+
+    public static function sendByLMT($numero, $message, $source, $username, $password)
+    {
 
         $url = 'http://lmtgroup.dyndns.org/sendsms/sendsmsGold.php?';
-                    $timeout = 10;
-                    $destination = $numero;
-                    $request = $url . "UserName=" . urlencode($username) . "&Password=" . urlencode($password) . "";
-                    $request .= "&SOA=" . urlencode($source) . "&MN=" . urlencode($destination) . "&SM=" . urlencode($message);
+        $timeout = 10;
+        $destination = $numero;
+        $request = $url . "UserName=" . urlencode($username) . "&Password=" . urlencode($password) . "";
+        $request .= "&SOA=" . urlencode($source) . "&MN=" . urlencode($destination) . "&SM=" . urlencode($message);
 
-                    $url = $request;
-                    $ch = curl_init();
-                    curl_setopt($ch, CURLOPT_URL, $url);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                    curl_setopt($ch, CURLOPT_POST, 1);
+        $url = $request;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
 
-                    curl_exec($ch);
-                    curl_close($ch);
+        curl_exec($ch);
+        curl_close($ch);
 
-                    return 'success';
-      }
+        return 'success';
+    }
 
-      public static function sendByMTARGET($numero,$message,$source,$username,$password) {
+    public static function sendByMTARGET($numero, $message, $source, $username, $password)
+    {
+        return self::sendByNexhas($numero, $message, $source, $username, $password);
+    }
 
-                    $destination = $numero; $source = urlencode($source);
-                    
-                    $message = urlencode($message);
+    public static function sendByNexhas($numero, $message, $source, $username, $password)
+    {
 
-                    $url = 'https://api-public.mtarget.fr/api-sms.json?username='.$username.'&password='.$password.'&sender='.$source.'&msisdn=%2b237'.$destination.'&msg='.$message;
-        
-                    $ch = curl_init();
-            
-                    curl_setopt($ch, CURLOPT_URL, $url);
-            
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            
-                    curl_exec($ch);
-        
-                    curl_close($ch);
+        $destination = $numero;
 
-                    return 'success';
-     }
+        $curl = curl_init();
 
-       public static function sendByNexhas($numero,$message,$source,$username,$password) {
-
-           $destination = $numero;
-
-           $curl = curl_init();
-
-           curl_setopt_array($curl, array(
-               CURLOPT_URL => 'https://smsvas.com/bulk/public/index.php/api/v1/sendsms',
-               CURLOPT_RETURNTRANSFER => true,
-               CURLOPT_ENCODING => '',
-               CURLOPT_MAXREDIRS => 10,
-               CURLOPT_TIMEOUT => 0,
-               CURLOPT_FOLLOWLOCATION => true,
-               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-               CURLOPT_CUSTOMREQUEST => 'POST',
-               CURLOPT_POSTFIELDS =>'{
-                  "user": "'.$username.'",
-                  "password": "'.$password.'",
-                  "senderid": "'.$source.'",
-                  "sms": "'.$message.'",
-                  "mobiles": "'.$destination.'"
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://smsvas.com/bulk/public/index.php/api/v1/sendsms',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+                  "user": "' . $username . '",
+                  "password": "' . $password . '",
+                  "senderid": "' . $source . '",
+                  "sms": "' . $message . '",
+                  "mobiles": "' . $destination . '"
               }',
-               CURLOPT_HTTPHEADER => array(
-                   'Content-Type: application/json'
-               ),
-           ));
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
 
-           $response = curl_exec($curl);
+        $response = curl_exec($curl);
 
-           curl_close($curl);
+        curl_close($curl);
 
-           return 'success';
-       }
-     
-   }
+        return 'success';
+    }
+
+}
+
 ?>
